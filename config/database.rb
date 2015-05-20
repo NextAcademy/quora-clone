194f83cfaaa6f642bdc9571d67283e4e8ffa46e6
Holloway, Kean Ho <hollowaykeanho@gmail.com>
2015-05-21 00:04:52 +0800

    added environment detection for setting up database
    Signed-off-by: Holloway, Kean Ho <hollowaykeanho@gmail.com>

--- config/database.rb
+++ config/database.rb
@@ -32,12 +32,20 @@ DB_NAME = db.path[1..-1]
 #		if ENV['RACK_ENV'] is set.  If ENV['RACK_ENV'] is not set, it defaults
 #		to :development
 
-ActiveRecord::Base.establish_connection(
-	:adapter	=> db.scheme == 'postgres' ? 'postgresql' : db.scheme,
-	# :host			=> db.host,
-	# :port			=> db.port,
-	# :username	=> db.user,
-	# :password	=> db.password,
-	:database => DB_NAME,
-	:encoding => 'utf8'
-)
+if Sinatra::Application.development?
+	ActiveRecord::Base.establish_connection(
+		adapter:	db.scheme == 'postgres' ? 'postgresql' : db.scheme,
+		database:	DB_NAME,
+		encoding:	'utf8'
+	)
+else
+	ActiveRecord::Base.establish_connection(
+		adapter:	db.scheme == 'postgres' ? 'postgresql' : db.scheme,
+		host:			db.host,
+		port:			db.port,
+		username:	db.user,
+		password:	db.password,
+		database:	DB_NAME,
+		encoding:	'utf8'
+	)
+end
