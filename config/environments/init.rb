f42345074824abab7a8d33308976184793ebac71
Holloway, Kean Ho <hollowaykeanho@gmail.com>
2015-10-08 09:50:17 +0800

    config: environments: init: fixed Issue #16
    The session was initialized twice. The latter overridden the secret
    key for the first initialization. This patch is created to remove the
    second initialization.
    
    Source:
    https://github.com/hollowaykeanho/sinatra-web-server/issues/16
    
    Signed-off-by: Holloway, Kean Ho <hollowaykeanho@gmail.com>

--- config/environments/init.rb
+++ config/environments/init.rb
@@ -31,11 +31,10 @@ APP_NAME = APP_ROOT.basename.to_s
 
 # Setup sessions, logging and dump_errors
 use Rack::Session::Cookie, expire_after: ENV['SESSION_EXPIRE'] || 2592000, # seconds
-                           secret: ENV['SESSION_SECRET'] || 'this is a secret shhhhh'
-set :logging, true
-set :dump_errors, false
-set :app_file, __FILE__
-enable :sessions
+                           secret: ENV['SESSION_SECRET'] || 'this is a secret shhhhh',
+                           logging: true,
+                           dump_errors: false,
+                           app_file: __FILE__
 
 # Setup Models (M) and its database
 require APP_ROOT.join('config', 'database')
