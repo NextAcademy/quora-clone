14e69a28fbaa41c0437602d8c046d131c1a1d0c6
Holloway, Kean Ho <hollowaykeanho@gmail.com>
2015-11-05 11:56:14 +0800

    enabled layout for erb in views
    Signed-off-by: Holloway, Kean Ho <hollowaykeanho@gmail.com>

--- config/environments/init.rb
+++ config/environments/init.rb
@@ -44,6 +44,7 @@ require APP_ROOT.join('config', 'database')
 
 # Setup views (V)
 set :views, File.join(APP_ROOT, "app", "views")
+set :erb, layout: :'layouts/application'
 
 # Setup helper (H)
 Dir[APP_ROOT.join('app', 'helpers', '*.rb')].each { |file| require file }
