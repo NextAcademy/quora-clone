3d3294cc2369799f9efcf1eb69f5126213e5b416
mkc306 <m3k0c6@gmail.com>
2016-12-15 17:21:31 +0800

    sessions now working

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,12 +2,34 @@ require 'sinatra/flash'
 require 'byebug'
 require 'bcrypt'
 
+enable :sessions
 
 get '/main' do
   erb :"user/main"
 end
 
-post '/main' do
+get '/' do
+  if logged_in? 
+    erb :"user/main"
+  else
+    erb :"static/home"
+  end
+end
+
+get '/profile' do
+  erb :"user/profile"
+end 
+
+get '/register' do
+  erb :"static/register"
+end
+
+get '/login' do
+  erb :"static/login"
+end
+
+
+post '/register' do
   user = User.new(params[:user])
   if user.save
     redirect '/'
@@ -22,8 +44,14 @@ post '/login' do
   user = User.find_by(username: params[:user]["username"])
   if user.authenticate(params[:user]["password"])
     puts "logged in"
+    session[:current_user_id] = user.id
+    byebug
     redirect '/main'
   else
     flash[:login] = user.errors.full_messages
   end
+end
+
+get '/logout' do
+  session.clear 
 end
