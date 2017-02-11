37e10d6a26e10509d0c959080a9cf7a19a551951
artze <arthurwtm@gmail.com>
2017-02-11 18:18:11 +0800

    added signup page and signup feature

--- app/controllers/static.rb
+++ app/controllers/static.rb
@@ -1,3 +1,25 @@
 get '/' do
   erb :"static/index"
 end
+
+get '/signup' do
+	erb :'static/signup'
+end
+
+post '/signup' do
+	user = User.new(email: params[:email], username: params[:username], password: params[:password])
+	user.save
+	if user.save
+		redirect '/signup/success'
+	else
+		redirect '/signup/failure'
+	end
+end
+
+get '/signup/success' do
+	erb :'static/signup-success'
+end
+
+get '/signup/failure' do
+	erb :'static/signup-failure'
+end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,6 @@
 class User < ActiveRecord::Base
 	# This is Sinatra! Remember to create a migration!
 	has_secure_password
-	validates :email, :password_digest, :username, presence: true
+	validates :email, :username, presence: true
 	validates :email, :username, uniqueness: true
 end
