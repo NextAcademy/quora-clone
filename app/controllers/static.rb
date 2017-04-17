6aa0ae1932849af46c62d9ab40f7439d879c086b
nhnaoto <nhnaoto.nh@gmail.com>
2017-04-17 17:07:07 +0800

    added main logic sign-up, log-in

--- app/controllers/static.rb
+++ app/controllers/static.rb
@@ -1,3 +1,32 @@
 get '/' do
   erb :"static/index"
 end
+
+post '/signup' do
+	user = User.new(params[:user])
+	if user.save
+		puts "saved"
+		redirect "/"
+	else
+		puts "error"
+	end
+end
+
+post '/login' do
+
+	user_db = User.find_by_email(params[:user][:email])
+
+	if user_db.password == params[:user][:password]
+		puts "authentication success"
+	else
+		puts "authentication failure"
+	end
+
+end
+
+
+
+
+
+
+
