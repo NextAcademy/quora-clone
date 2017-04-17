12e669c4b1c14990e080ffe7ea8e717f6970f7f4
Michelle  Ler Hsin Yee <MichelleLer@Michelles-MacBook-Pro.local>
2017-04-17 17:13:54 +0800

    Sign up is working and created login.erb

--- app/controllers/static.rb
+++ app/controllers/static.rb
@@ -12,34 +12,38 @@ get '/signup' do
 end
 
 post '/signup' do
-   @user = User.new(params[:user])
+   user = User.new(params[:user])
 
   if user.save
-    # what should happen if the user is saved?
-    p "<h1>Account successfully created!</h1><a href='../login'>login</a>"
-    redirect '/'
+    redirect "users/#{user.id}"
   else
-    # what should happen if the user keyed in invalid date?
-    p "Error"
-    erb :"static/index"
+    erb :"static/signup"
   end
-end  
-
-get '/login' do
-  erb :"static/login"
 end
 
-post '/login' do
-  # apply a authentication method to check if a user has entered a valid email and password
-  # if a user has successfully been authenticated, you can assign the current user id to a session
+get '/users/:id' do
+  # some code here
+  @user = User.find_by_id(params[:id])
+  erb :"users/profile"
 end
 
-get '/logout' do
-  session.delete("user")
-  redirect to("/")
-end
+# get '/login' do
+#   erb :"static/login"
+# end
 
-post '/logout' do
-  # kill a session when a user chooses to logout, for example, assign nil to a session
-  # redirect to the appropriate page
-end
+# post '/login' do
+#  user = User.find_by_email(params[:user][:email])
+
+#   # apply a authentication method to check if a user has entered a valid email and password
+#   # if a user has successfully been authenticated, you can assign the current user id to a session
+# end
+
+# get '/logout' do
+#   session.delete("user")
+#   redirect to("/")
+# end
+
+# post '/logout' do
+#   # kill a session when a user chooses to logout, for example, assign nil to a session
+#   # redirect to the appropriate page
+# end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,21 @@
 class User < ActiveRecord::Base
 	# This is Sinatra! Remember to create a migration!
-	has_secure_password
+  validates :email, uniqueness: true, presence: true
+  validates :full_name, presence: true
+  #validate :password_validator, :email_validator
+  before_create :password_validator, :email_validator
+  has_secure_password
+
+  def password_validator
+      unless self.password.length < 6
+        errors.add(:password, "is too short")
+      end
+  end
+
+  def email_validator
+     unless self.email =~ /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i
+       errors.add(:email, "is not valid")
+     end
+  end
 
 end
