498f648fabcaf33f074779d5515620396a17c99a
hewrin <hewrin@hotmail.com>
2017-04-18 11:45:06 +0800

    Add User model

---
+++ app/models/user.rb
@@ -0,0 +1,3 @@
+class User < ActiveRecord::Base
+	# This is Sinatra! Remember to create a migration!
+end
---
+++ spec/user_spec.rb
@@ -0,0 +1,10 @@
+# Requiring test environment file
+require 'spec_helper'
+
+# Requiring test subject file. Uncomment below.
+# require_relative File.join(APP_CONTROLLER, "static")
+
+# Test cases
+describe 'User' do
+	pending "pending test cases"
+end
