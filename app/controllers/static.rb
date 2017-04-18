40dc6dcdec5fbcee1cecd2e022bb26f9588d7019
Michelle  Ler Hsin Yee <MichelleLer@Michelles-MacBook-Pro.local>
2017-04-18 11:22:28 +0800

    added comments on restful routes on static.rb

--- app/controllers/static.rb
+++ app/controllers/static.rb
@@ -57,6 +57,15 @@ end
 #do not go through 2 levels of models 
 #payments belongs_to transaction
 
+# Between 2 levels
+# http verb          resource/path              restful action
+# get			/books/1/authors/1		              show
+# get			/books/1/authors/new			      new
+# post		/books/1/authors			         create
+# get			/books			                      index
+# get			/magazines/1/authors/1/edit		       edit
+# put/patch	/books/1/authors/1			          update
+# delete		/books/1/authors/1	                 destroy
 
 
 
