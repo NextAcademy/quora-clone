7580c6628734c83bf1391b9ad21521eba79830b7
sofiaadam <sofiaadamkho@gmail.com>
2017-04-18 14:35:06 +0800

    ..

--- app/controllers/static.rb
+++ app/controllers/static.rb
@@ -2,16 +2,3 @@ get '/' do
   erb :"static/index"
 end
 
-# get '/signup' do
-#   erb :"static/signup"
-# end
-
-
-# get '/login' do
-#   erb :"static/login"
-# end
-
-# get 'users/:id' do
-#   erb :"static/profile"
-# end
-
