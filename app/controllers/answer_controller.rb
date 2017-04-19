e5fbfa247df89a348b29163a58cd3ae136904eb7
Michelle  Ler Hsin Yee <MichelleLer@Michelles-MacBook-Pro.local>
2017-04-19 12:57:14 +0800

    fixed all errors so everything is working

--- app/controllers/answer_controller.rb
+++ app/controllers/answer_controller.rb
@@ -6,19 +6,23 @@ post '/question/:id/answer' do
   answer.question_id = @question.id
   answer.user_id = current_user.id
   if answer.save
-    redirect '/question/#{params[:id]}'
+    redirect "/question/#{params[:id]}"
   else
     flash[:msg] = "ERROR"
   end
 end
 
-get '/question/:id/answers' do
-  @question = Question.find(params[:id])
-  erb :"questions/index"
+get '/users/:id/answers' do
+  @user = User.find(params[:id])
+  @answers = Answer.where(params[:id])
+  erb :"questions/answers"
 end
+
+# get '/question/:id/answers' do
+#   @question = Question.find_by_id(params[:id])
+#   @answers = Answer.where(params[:id])
+#   erb :"questions/index"
+# end
  
-get '/question/:q_id/answer/:a_id' do
-  @question = Question.find(params[:q_id])
-  erb :"questions/show"
-end
+
 
