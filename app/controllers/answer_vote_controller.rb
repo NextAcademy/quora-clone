7027b1bd65de2a29c319f26c8a28d65dcbf4d0a5
Michelle  Ler Hsin Yee <MichelleLer@Michelles-MacBook-Pro.local>
2017-04-20 09:45:29 +0800

    upvote button

--- app/controllers/answer_vote_controller.rb
+++ app/controllers/answer_vote_controller.rb
@@ -1,5 +1,21 @@
 enable :sessions
 
 post "/answer/:id/upvote" do
-  
+  # @question = Question.find(params[:id])
+  # current_vote = QuestionVote.where(user_id: current_user.id, question_id: params[:id]).first
+  current_vote = current_user.answer_votes.find_by(answer_id: params[:id])
+  if current_vote.nil?
+    @vote = current_user.answer_votes.new(answer_id: params[:id], vote_type: true)
+    @vote.save
+    redirect "/"
+  else
+    # current_vote.destroy
+    if current_vote.vote_type
+      current_vote.update_attribute(:vote_type, false) #==> update_attribute will not stop by callbacks
+    elsif !current_vote.vote_type
+      current_vote.update_attribute(:vote_type, true)
+    end
+    redirect "/"
+  end
 end
+
