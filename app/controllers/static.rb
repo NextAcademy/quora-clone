6339184bafa14366e7a67f5a01db7ce5fcc8390f
leesc22 <leesiewching22@gmail.com>
2017-09-07 14:14:09 +0800

    Set up users controller template to POST signup

--- app/controllers/static.rb
+++ app/controllers/static.rb
@@ -1,3 +1,18 @@
+require 'byebug'
+
 get '/' do
   erb :"static/index"
+end
+
+post '/signup' do
+	# feature specific to Rails/Sinatra
+	# when the form input name is in the format of name="user[attr]"
+	# we can use User.new(params[:user])
+	# form value is automatically transformed into a hash
+	user = User.new(params[:user])
+	if user.save
+		# codes
+	else
+		# codes
+	end
 end
