196d5453e4df5c9c9034a17c2e0accc52ba1d0c6
leesc22 <leesiewching22@gmail.com>
2017-09-07 12:07:37 +0800

    Adding validation into User model file

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,21 @@
+require 'uri'
+
 class User < ActiveRecord::Base
-	# This is Sinatra! Remember to create a migration!
+	validates :name, :email, :password, presence: true
+	validates :email, uniqueness: true
+	validates :email, format: { with: URI::MailTo::EMAIL_REGEXP, message: "Invalid email format!" }
+	validates :password, length: { minimum: 6 }
+	has_secure_password
 end
+
+# Reference
+# ==> Email validation
+#     - http://yogodoshi.com/ruby-already-has-its-own-regular-expression-to-validate-emails/
+
+# ==> bcrypt SecurePassword
+#     - https://medium.com/@tpstar/password-digest-column-in-user-migration-table-871ff9120a5
+
+# Driver code
+# user = User.new(name: 'David', email: 'david', password: 'david')
+# user.save
+# user.errors.messages
