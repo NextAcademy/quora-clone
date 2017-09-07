5f77f9f0e97fc285e523982ee9facf860ee03e64
Hui Qian Liang <liang.huiqian@gmail.com>
2017-09-07 17:41:12 +0800

    Added User, Question and Answer models

---
+++ app/models/answer.rb
@@ -0,0 +1,3 @@
+class Answer < ActiveRecord::Base[5.0]
+	# This is Sinatra! Remember to create a migration!
+end
---
+++ app/models/question.rb
@@ -0,0 +1,3 @@
+class Question < ActiveRecord::Base[5.0]
+	# This is Sinatra! Remember to create a migration!
+end
