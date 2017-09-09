678da4836b18186d35df1241f139313a4cf758d4
GeorgeSalari <x3m.giku@gmail.com>
2017-09-10 01:02:32 +0800

    fix css of profile, create question with all answers page, finish css for this page

--- app/models/answer.rb
+++ app/models/answer.rb
@@ -1,6 +1,7 @@
 class Answer < ActiveRecord::Base
 	# This is Sinatra! Remember to create a migration!
   validates :content, presence: true
+  belongs_to :user
 
   def upvote_answer
     self.update(votes_count: self.votes_count + 1)
