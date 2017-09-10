748ca2df94c678989bc59474ef5ebe4c7e94d326
leesc22 <leesiewching22@gmail.com>
2017-09-10 09:03:40 +0800

    Update controller to save vote into database

--- app/controllers/question_votes_controller.rb
+++ app/controllers/question_votes_controller.rb
@@ -1,8 +1,11 @@
+require 'byebug'
+
 post '/question_votes' do
 	# puts params
-	# 
+	# {"question_vote"=>{"vote"=>"1", "user_id"=>"1", "question_id"=>"2"}, "captures"=>[]}
+	# {"question_vote"=>{"vote"=>"-1", "user_id"=>"1", "question_id"=>"2"}, "captures"=>[]}
 
-	question_votes = QuestionVote.new(params[:question_votes])
+	question_votes = QuestionVote.new(params[:question_vote])
 	if question_votes.save
 		redirect back
 	else
