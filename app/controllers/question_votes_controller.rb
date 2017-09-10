83ee719539bf4abbf83625e68a32cd88bba4cf85
leesc22 <leesiewching22@gmail.com>
2017-09-10 20:47:35 +0800

    Validate user can only upvote or downvote once at question page

--- app/controllers/question_votes_controller.rb
+++ app/controllers/question_votes_controller.rb
@@ -13,17 +13,27 @@ post '/question_votes' do
 		question_votes = QuestionVote.new(params[:question_vote])
 	else
 		question_votes = user_voted
-		question_votes.vote += params[:question_vote][:vote].to_i
+		param_vote = params[:question_vote][:vote].to_i
+		unless question_votes.vote == param_vote
+			question_votes.vote += param_vote
+		end
 	end
 
 	if question_votes.save
 		redirect back
 	else
 		@question_votes_errors = question_votes.errors.messages
+		# @user = User.all
+		# @questions = Question.all
+		# @answers = Answer.all
+		# @question_votes = QuestionVote.all
+		# erb :"static/index"
+
+		@question = Question.find_by_id(@question_id)
+		@title = @question.title + " - Quora Clone"
+		@answers = Answer.where(question_id: @question.id).order(created_at: :desc)
 		@user = User.all
-		@questions = Question.all
-		@answers = Answer.all
-		@question_votes = QuestionVote.all
-		erb :"static/index"
+		@question_votes = QuestionVote.where(question_id: @question.id).sum(:vote)
+		erb :"static/question"	
 	end
 end
