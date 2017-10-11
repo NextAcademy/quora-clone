a1ea2367a3a6c68e378b4a352f23f4b3814c7c23
zyang95 <leetzyyang@gmail.com>
2017-10-11 09:47:15 +0800

    i tried to do authentication but i failed but i did login and signup page

--- app/controllers/static.rb
+++ app/controllers/static.rb
@@ -1,3 +1,39 @@
 get '/' do
-  erb :"static/index"
+  erb :"users/login"
+end
+
+
+get '/signup' do 
+	erb :"users/signup"
+end
+
+post '/new' do 
+	newuser = User.new(params[:user])
+	newuser.save
+	if newuser.save
+		
+		@error = "you may now log in"
+		redirect '/'
+	else
+		@error = "please try again"
+		redirect "/signup"
+	end
+end
+
+post '/login' do
+ 	user = User.find_by(params[:user])
+ 	if params[:user][:password_digest] = user.password_digest
+ 		redirect '/user/#{user.id}'
+ 	else
+ 		@error = "invalid username/password"
+ 		redirect '/'
+ 	end
+end
+
+post '/logout' do 
+	redirect = '/'
+end
+
+get '/user/:id' do
+	redirect = '/'
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,7 @@
 class User <ActiveRecord::Base
+	has_secure_password
+	validates :full_name, :email, :password_digest, presence: true
+	validates :email, uniqueness: {case_sensitive: false, message: "this email has been used before"}
+	validates :email, format: { with: /\w+@\w+\.\w+/,
+    message: "only emails allowed" }
 end
