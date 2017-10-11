564c29d384dee934c11a92b2dab5f1c95d57169e
shinomatsu <shi968k5@gmail.com>
2017-10-12 01:31:58 +0800

    in passward,how to validate is changed. message is changed.

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,8 +2,11 @@ class User < ActiveRecord::Base
 	# This is Sinatra! Remember to create a migration!
 	
 	has_secure_password
-
+     validates :email, presence: true
+	 validates :email, uniqueness: {case_sensitive: false, message: "was used before"}
+	 validates :email, format: { with: /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i}
+	 validates :password, presence: true
+	 validates :password, format: { with: /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]{6}/, message:"should be more than six digits in length and contain both letters and numbers"}
 	 
 
-	 validates :email, uniqueness: true, format: { with: /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i}
 end
