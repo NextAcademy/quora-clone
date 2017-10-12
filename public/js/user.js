d4498f926445dafe7723565ee12521472b321cfc
Aryailia <Aryailia@users.noreply.github.com>
2017-10-12 11:11:59 +0800

    fixed removal of question popup

--- public/js/user.js
+++ public/js/user.js
@@ -2,31 +2,38 @@
   'use strict';
 
   $(document).ready(function () {
+    var $body = $(document.body);
     $('#askquestion').click(function () {
       var $cover = $('<div>').addClass('cover');
-      //console.log($cover);
-
       var $button, $content;
-      var $box =
+      var $askComponent =
         $('<div class="ask">')
           .append($('<div class="container">')
             .append($content = $('<textarea>'))
           )
           .append($button = $('<div><input type="submit">'));
+
+      function removeAskBox() {
+        $cover.remove();
+        $askComponent.remove();
+      }
   
-      $button.click(function (a) {
-        $.ajax({
+      $button.click(function (ev) {
+        $.ajax({ // Submit message
           url: '/askquestion',
           method: 'POST',
           data: { content: $content.val() },
           success: function (msg) {
-            $(document.body).remove($cover).remove($box);
+            //if (receive error) // When have time to do error handling
+            removeAskBox();
           }
         })
       });
+      
+      $cover.click(removeAskBox);
 
-      $(document.body).append($cover);
-      $(document.body).append($box);
+      $body.append($cover);
+      $body.append($askComponent);
     });
   });
 })();
