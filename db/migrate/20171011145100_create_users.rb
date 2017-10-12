e9664593d26361eca6798b974e9c93863e7327fe
sophieyang0813@gmail.com <sophieyang@Sophies-MacBook-Pro.local>
2017-10-12 10:55:07 +0800

    password and email validation in user model file

--- db/migrate/20171011145100_create_users.rb
+++ db/migrate/20171011145100_create_users.rb
@@ -1,6 +1,6 @@
-class CreateUsers < ActiveRecord::Migration
+class CreateUsers < ActiveRecord::Migration[5.0]
     def change
-        create_table: users do |t|
+        create_table :users do |t|
             t.string :name
             t.string :email
             t.string :password_digest
