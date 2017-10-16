a58fd632c55836d4c05723af2c9545c87f95673f
zyang95 <leetzyyang@gmail.com>
2017-10-16 14:29:24 +0800

    hello

--- app/models/answer.rb
+++ app/models/answer.rb
@@ -1,10 +1,5 @@
 class Answer <ActiveRecord::Base
-<<<<<<< HEAD
-  belongs_to :question
-  belongs_to :user
-=======
 	belongs_to :question
 	belongs_to :user
 	has_many  :answer_votes
->>>>>>> master
 end
