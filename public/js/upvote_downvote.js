e12b847c4396fe09331ba1a3a3d16341d4ba4de5
zyang95 <leetzyyang@gmail.com>
2017-10-16 19:05:46 +0800

    small hands

--- public/js/upvote_downvote.js
+++ public/js/upvote_downvote.js
@@ -1,47 +1,60 @@
 
 $(document).ready(function(){
-		
-	document.querySelectorAll(".counter_state").forEach(function (elem) {
+	document.querySelectorAll(".answer_counter_state").forEach(function (elem) {
 	  	if ($(elem).val() == 1) {
-				$(elem).parent().find('.upvote_button').css("background", "grey")
+				$(elem).parent().find('.answer_upvote_button').css("background", "grey")
 			} else if ($(elem).val() == '0') {
-				$(elem).parent().find('.upvote_button').css("background", "white")
-				$(elem).parent().find('.downvote_button').css("background", "white")
+				$(elem).parent().find('.answer_upvote_button').css("background", "white")
+				$(elem).parent().find('.answer_downvote_button').css("background", "white")
 			} else if ($(elem).val() == -1) 
-				$(elem).parent().find('.downvote_button').css("background", "grey")
+				$(elem).parent().find('.answer_downvote_button').css("background", "grey")
+	});
+
+	$('.answer_upvote_button').click(function(e){
+		var $target = $(e.currentTarget);
+		var $target_downvote = $target.parent().find('.answer_downvote_button')
+		var y = $target.parent().find('.answer_id').val();
+		var $counter=".vote_counter_answer"
+
+		upvote("/upvote_answer", y, $target, $target_downvote, $counter, "/vote_status_answer")
+	});
+
+
+	$('.answer_downvote_button').click(function(e){
+		var $target = $(e.currentTarget)
+		var $target_upvote = $target.parent().find('.answer_upvote_button')
+		var y = $target.parent().find('.answer_id').val();
+		var $counter=".vote_counter_answer"
+		
+		downvote("/downvote_answer", y, $target, $target_upvote, $counter, "/vote_status_answer")
+	});
+
+	document.querySelectorAll(".counter_state").forEach(function (elem) {
+		if ($(elem).val() == 1) {
+			$(elem).parent().find('.upvote_button').css("background", "grey")
+		} else if ($(elem).val() == '0') {
+			$(elem).parent().find('.upvote_button').css("background", "white")
+			$(elem).parent().find('.downvote_button').css("background", "white")
+		} else if ($(elem).val() == -1) 
+			$(elem).parent().find('.downvote_button').css("background", "grey")
 	});
 
 	$('.upvote_button').click(function(e){
 		var $target = $(e.currentTarget);
 		var $target_downvote = $target.parent().find('.downvote_button')
-		var y = $target.parent().find('.question_id_upvote').val();
+		var y = $target.parent().find('.question_id').val();
+		var $counter = '.vote_counter'
 
-		upvote("/upvote_question", y, $target, $target_downvote)
+		upvote("/upvote_question", y, $target, $target_downvote, $counter, "/vote_status")
 	});
-
 
 	$('.downvote_button').click(function(e){
-		var $target = $(e.currentTarget)
-		var $target_upvote = $target.parent().find('.upvote_button')
-		var y = $target.parent().find('.question_id_upvote').val();
-		
-		downvote("/downvote_question", y, $target, $target_upvote)
-	});
-
-	$('.upvote_button_answer').click(function(e){
 		var $target = $(e.currentTarget);
-		var $target_downvote = $target.parent().find('.downvote_button_answer')
-		var y = $target.parent().find('.answer_id').val();
+		var $target_downvote = $target.parent().find('.upvote_button')
+		var y = $target.parent().find('.question_id').val();
+		var $counter = '.vote_counter'
 
-		upvote("/upvote_answer", y, $target, $target_downvote)
-	});
-
-	$('.downvote_button_answer').click(function(e){
-		var $target = $(e.currentTarget);
-		var $target_downvote = $target.parent().find('.upvote_button_answer')
-		var y = $target.parent().find('.answer_id').val();
-
-		downvote("/downvote_answer", y, $target, $target_downvote)
+		downvote("/downvote_question", y, $target, $target_downvote, $counter, "/vote_status")
 	});
 });
 
@@ -49,7 +62,7 @@ $(document).ready(function(){
 
 
 
-function upvote(link, id_of_question_answer, $button, $opposite_button){
+function upvote(link, id_of_question_answer, $button, $opposite_button, $vote_counter, $link_2){
 	$.post(link, 
 		{ question_answer_id: id_of_question_answer
 		},
@@ -57,8 +70,8 @@ function upvote(link, id_of_question_answer, $button, $opposite_button){
 			if (data == "error") {
 				window.location.assign("http://localhost:9393/")
 			} else {
-			$button.parent().find('.vote_counter').text(data);
-				$.post("/vote_status",
+			$button.parent().find($vote_counter).text(data);
+				$.post($link_2,
 					{ question_answer_id: id_of_question_answer
 					},
 					function (y) {
@@ -79,7 +92,7 @@ function upvote(link, id_of_question_answer, $button, $opposite_button){
 	);
 };
 
-function downvote(link, id_of_question_answer, $button, $opposite_button){
+function downvote(link, id_of_question_answer, $button, $opposite_button, $vote_counter, $link_2){
 	$.post(link, 
 		{ question_answer_id: id_of_question_answer
 		},
@@ -87,8 +100,8 @@ function downvote(link, id_of_question_answer, $button, $opposite_button){
 			if (data == "error") {
 				window.location.assign("http://localhost:9393/")
 			} else {	
-			$button.parent().find('.vote_counter').text(data)
-					$.post("/vote_status",
+			$button.parent().find($vote_counter).text(data)
+					$.post($link_2,
 					{ question_answer_id: id_of_question_answer
 					},
 					function (y) {
