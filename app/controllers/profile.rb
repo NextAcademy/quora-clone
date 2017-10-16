a3549e5a8c571e3d0816335d8e60b889cc55a1a9
zyang95 <leetzyyang@gmail.com>
2017-10-16 14:55:56 +0800

    u suck

--- app/controllers/profile.rb
+++ app/controllers/profile.rb
@@ -5,3 +5,10 @@ end
 get '/profile/:id/answers' do
 	erb :'users/profile_answers', layout: :'layouts/profile'
 end
+
+get '/profile' do
+	if logged_in?
+		p "","","","hello","","/profile/#{session[:user_id]}"
+		redirect("/profile/#{session[:user_id]}")
+	end
+end
