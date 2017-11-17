417ff3b58f1d577d0b5c6b6419fec2dff312974d
KahTim <kahtim.tang@gmail.com>
2017-11-17 08:49:10 +0800

    log out added

--- app/controllers/sessions.rb
+++ app/controllers/sessions.rb
@@ -1,4 +1,4 @@
-post '/sessions' do 
+post '/login' do 
 	user = User.find_by(email:params[:email])
 	if user.authenticate(params[:password])
 		session[:user_id] = user.id
@@ -6,4 +6,9 @@ post '/sessions' do
 	else
 		redirect '/'
 	end
+end
+
+get '/logout' do 
+	session[:user_id] = nil
+	erb :"static/index"
 end
