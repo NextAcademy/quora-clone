d8e63c64cc5de1bda88dce5d23c19496a2a851b9
KahTim <kahtim.tang@gmail.com>
2017-11-20 08:25:28 +0800

    fixed logout bug

---
+++ app/models/question_vote.rb
@@ -0,0 +1,3 @@
+class QuestionVote < ActiveRecord::Base[5.0]
+	# This is Sinatra! Remember to create a migration!
+end
---
+++ db/migrate/20171120082518_create_answer_votes.rb
@@ -0,0 +1,4 @@
+class CreateAnswerVotes < ActiveRecord::Migration
+	def change
+	end
+end
